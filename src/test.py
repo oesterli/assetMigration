d5dc0c44b86e433abf332dc97c7529a4c65e5d5b
Oesterling Nils <Nils.Oesterling@swisstopo.ch>
2022-08-05 10:25:50 +0200

    test

--- src/test.py
+++ src/test.py
@@ -1,1 +1,8 @@
+## import libraries
+
+import pandas as pd
+
 print("test")
+
+
+inPath = r"M:\Appl\DATA\PROD\lg\_restricted\_TP5_TEST-Migration\in\ctrlLog\01_Filename-Overview__kontr-Daten_2022-06-28_15-12-42_bearbPFI.xlsx"
